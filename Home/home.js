1d8c688a036940c486e22e317d9a4cd6f008fb33
Venkatesan R S <wenkatesan@gmail.com>
2023-03-18 21:37:13 +0530

    today popup try
    chatgpt try

--- Home/home.js
+++ Home/home.js
@@ -3,10 +3,48 @@ const openModal = document.querySelector(".login-button")
 const closeModal = document.querySelector(".close-button")
 
 openModal.addEventListener('click', () => {
+   console.log("dialog opened")
     modal.showModal();
 })
 
 closeModal.addEventListener('click', () => {
+    console.log("dialog closed")
     modal.close();
 })
+//changes made to exisiting js file below....
+var dialog = document.getElementById('dialog');
 
+document.addEventListener('click', function(event) {
+  if (!dialog.contains(event.target)) {
+    //dialog.close();
+    console.log("dialog closed because of chatgpt")
+  }
+});
+//div popup and close
+/*
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  function hidePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
+  */
+  function showPopup() {
+    document.getElementById("popup").style.display = "block";
+    window.addEventListener("click", hidePopupOutside);
+  }
+  
+  function hidePopup() {
+    document.getElementById("popup").style.display = "none";
+    window.removeEventListener("click", hidePopupOutside);
+  }
+  
+  function hidePopupOutside(event) {
+    const popup = document.getElementById("popup");
+    if (!popup.contains(event.target)) {
+      //popup.style.display = "none";
+      window.removeEventListener("click", hidePopupOutside);
+    }
+  }
+  
